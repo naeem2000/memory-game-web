b6af07f7d64c245ba62968d7ce6ed83993c9160b
Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-03-19 22:20:56 +0200

    started on home screen

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './game.scss';
 
 export default function Page() {
 	return <section>Game</section>;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import './home.scss';
+
 export default function Home() {
-	return <>Home</>;
+	return (
+		<>
+			<section className='home'>
+				<div className='header'>
+					<div>
+						<h1>Memory</h1>
+						<button>Exit Game</button>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
