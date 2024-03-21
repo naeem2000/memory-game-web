66c737131813d9399c74a38c8ebd1ae5cc33aac9
Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-03-21 18:39:28 +0200

    added cards

---
+++ src/app/data.ts
@@ -0,0 +1,122 @@
+//clover
+import clovera from './assets/cards/cloverA.svg';
+import clover2 from './assets/cards/clover2.svg';
+import clover3 from './assets/cards/clover3.svg';
+import clover4 from './assets/cards/clover4.svg';
+import clover5 from './assets/cards/clover5.svg';
+import clover6 from './assets/cards/clover6.svg';
+import clover7 from './assets/cards/clover7.svg';
+import clover8 from './assets/cards/clover8.svg';
+import clover9 from './assets/cards/clover9.svg';
+import clover10 from './assets/cards/clover10.svg';
+import cloverj from './assets/cards/cloverJ.svg';
+import cloverq from './assets/cards/cloverQ.svg';
+import cloverk from './assets/cards/cloverK.svg';
+
+//diamond
+import diamonda from './assets/cards/diamondA.svg';
+import diamond2 from './assets/cards/diamond2.svg';
+import diamond3 from './assets/cards/diamond3.svg';
+import diamond4 from './assets/cards/diamond4.svg';
+import diamond5 from './assets/cards/diamond5.svg';
+import diamond6 from './assets/cards/diamond6.svg';
+import diamond7 from './assets/cards/diamond7.svg';
+import diamond8 from './assets/cards/diamond8.svg';
+import diamond9 from './assets/cards/diamond9.svg';
+import diamond10 from './assets/cards/diamond10.svg';
+import diamondj from './assets/cards/diamondJ.svg';
+import diamondq from './assets/cards/diamondQ.svg';
+import diamondk from './assets/cards/diamondK.svg';
+
+//heart
+import hearta from './assets/cards/heartA.svg';
+import heart2 from './assets/cards/heart2.svg';
+import heart3 from './assets/cards/heart3.svg';
+import heart4 from './assets/cards/heart4.svg';
+import heart5 from './assets/cards/heart5.svg';
+import heart6 from './assets/cards/heart6.svg';
+import heart7 from './assets/cards/heart7.svg';
+import heart8 from './assets/cards/heart8.svg';
+import heart9 from './assets/cards/heart9.svg';
+import heart10 from './assets/cards/heart10.svg';
+import heartj from './assets/cards/heartJ.svg';
+import heartq from './assets/cards/heartQ.svg';
+import heartk from './assets/cards/heartK.svg';
+
+//spade
+import spadea from './assets/cards/spadeA.svg';
+import spade2 from './assets/cards/spade2.svg';
+import spade3 from './assets/cards/spade3.svg';
+import spade4 from './assets/cards/spade4.svg';
+import spade5 from './assets/cards/spade5.svg';
+import spade6 from './assets/cards/spade6.svg';
+import spade7 from './assets/cards/spade7.svg';
+import spade8 from './assets/cards/spade8.svg';
+import spade9 from './assets/cards/spade9.svg';
+import spade10 from './assets/cards/spade10.svg';
+import spadej from './assets/cards/spadeJ.svg';
+import spadeq from './assets/cards/spadeQ.svg';
+import spadek from './assets/cards/spadeK.svg';
+
+//joker
+import jokerblack from './assets/cards/Jokerblack.svg';
+import jokerred from './assets/cards/Jokerred.svg';
+
+export const cards = {
+	card: [
+		{ id: 0, img: clovera, alt: 'blackAce' },
+		{ id: 1, img: clover2, alt: 'black2' },
+		{ id: 2, img: clover3, alt: 'black3' },
+		{ id: 3, img: clover4, alt: 'black4' },
+		{ id: 4, img: clover5, alt: 'black5' },
+		{ id: 5, img: clover6, alt: 'black6' },
+		{ id: 6, img: clover7, alt: 'black7' },
+		{ id: 7, img: clover8, alt: 'black8' },
+		{ id: 8, img: clover9, alt: 'black9' },
+		{ id: 9, img: clover10, alt: 'black10' },
+		{ id: 10, img: cloverj, alt: 'blackJack' },
+		{ id: 11, img: cloverq, alt: 'blackQueen' },
+		{ id: 12, img: cloverk, alt: 'blackKing' },
+		{ id: 13, img: diamonda, alt: 'redAce' },
+		{ id: 14, img: diamond2, alt: 'red2' },
+		{ id: 15, img: diamond3, alt: 'red3' },
+		{ id: 16, img: diamond4, alt: 'red4' },
+		{ id: 17, img: diamond5, alt: 'red5' },
+		{ id: 18, img: diamond6, alt: 'red6' },
+		{ id: 19, img: diamond7, alt: 'red7' },
+		{ id: 20, img: diamond8, alt: 'red8' },
+		{ id: 21, img: diamond9, alt: 'red9' },
+		{ id: 22, img: diamond10, alt: 'red10' },
+		{ id: 23, img: diamondj, alt: 'redJack' },
+		{ id: 24, img: diamondq, alt: 'redQueen' },
+		{ id: 25, img: diamondk, alt: 'redKing' },
+		{ id: 26, img: hearta, alt: 'redAce' },
+		{ id: 27, img: heart2, alt: 'red2' },
+		{ id: 28, img: heart3, alt: 'red3' },
+		{ id: 29, img: heart4, alt: 'red4' },
+		{ id: 30, img: heart5, alt: 'red5' },
+		{ id: 31, img: heart6, alt: 'red6' },
+		{ id: 32, img: heart7, alt: 'red7' },
+		{ id: 33, img: heart8, alt: 'red8' },
+		{ id: 34, img: heart9, alt: 'red9' },
+		{ id: 35, img: heart10, alt: 'red10' },
+		{ id: 36, img: heartj, alt: 'redJack' },
+		{ id: 37, img: heartq, alt: 'redQueen' },
+		{ id: 38, img: heartk, alt: 'redKing' },
+		{ id: 39, img: spadea, alt: 'blackAce' },
+		{ id: 40, img: spade2, alt: 'black2' },
+		{ id: 41, img: spade3, alt: 'black3' },
+		{ id: 42, img: spade4, alt: 'black4' },
+		{ id: 43, img: spade5, alt: 'black5' },
+		{ id: 44, img: spade6, alt: 'black6' },
+		{ id: 45, img: spade7, alt: 'black7' },
+		{ id: 46, img: spade8, alt: 'black8' },
+		{ id: 47, img: spade9, alt: 'black9' },
+		{ id: 48, img: spade10, alt: 'black10' },
+		{ id: 49, img: spadej, alt: 'blackJack' },
+		{ id: 50, img: spadeq, alt: 'blackQueen' },
+		{ id: 51, img: spadek, alt: 'blackKing' },
+		{ id: 52, img: jokerblack, alt: 'joker' },
+		{ id: 53, img: jokerred, alt: 'joker' },
+	],
+};
--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -4,9 +4,14 @@ import React, { useEffect, useState } from 'react';
 import { CurrentPlayers } from '@/modules/modules';
 import Header from '../Components/Header';
 import './game.scss';
+import { cards } from '../data';
+import facedDown from '../assets/cards/Card_Back.svg';
+import Image from 'next/image';
 
 export default function Page() {
 	const [players, setPlayers] = useState<CurrentPlayers>();
+	const [board, setBoard] = useState(cards.card);
+	const [flippedCards, setFlippedCards] = useState<number[]>([]);
 
 	useEffect(() => {
 		const userString = localStorage.getItem('users');
@@ -15,13 +20,49 @@ export default function Page() {
 			setPlayers(user);
 		}
 	}, []);
-	console.log(players);
+
+	const handleCardClick = (index: number) => {
+		if (flippedCards.includes(index)) return;
+
+		setFlippedCards((prevFlippedCards) => [...prevFlippedCards, index]);
+
+		if (flippedCards.length === 1) {
+			const firstCardIndex = flippedCards[0];
+			const secondCardIndex = index;
+
+			if (board[firstCardIndex].id === board[secondCardIndex].id) {
+				console.log('Match!');
+
+				setBoard((prevBoard) =>
+					prevBoard.filter(
+						(card, i) => i !== firstCardIndex && i !== secondCardIndex
+					)
+				);
+			} else {
+				setTimeout(() => {
+					setFlippedCards([]);
+				}, 1000);
+			}
+		}
+	};
 
 	return (
-		<section>
+		<section className='game'>
 			<Header />
-			{players && <p>{players.player1}</p>}
-			{players && <p>{players.player2}</p>}
+			<div className='game-body'>
+				<div className='player1'>1</div>
+				<div className='board'>
+					{board.map((card, index) => (
+						<div key={index} onClick={() => handleCardClick(index)}>
+							<Image
+								src={flippedCards.includes(index) ? card.img : facedDown}
+								alt='card'
+							/>
+						</div>
+					))}
+				</div>
+				<div className='player2'>2</div>
+			</div>
 		</section>
 	);
 }
